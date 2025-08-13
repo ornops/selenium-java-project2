9cb0ef8dae7ee597aebf2a0407352bc8848d7c06
ornops <joshuaornopia@gmail.com>
2025-08-13 22:04:22 +0800

    Updated Java file

--- SeleniumProject1/src/SeleniumPractice1.java
+++ SeleniumProject1/src/SeleniumPractice1.java
@@ -4,8 +4,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumPractice1{
 
 	public static void main(String[] args) {
-//		System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+		
 
 	}
 
